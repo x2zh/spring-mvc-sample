ecd43e5039e374edebc6ac8b74745e6e6e06b35d
zhangxiaoxiao005 <zhangxiaoxiao005@ke.com>
2019-03-06 03:20:25 +0800

    update 添加dubbo demo service

---
+++ src/main/java/net/zhangxiaoxiao/spring/mvc/learn/service/DubboDemoService.java
@@ -0,0 +1,5 @@
+package net.zhangxiaoxiao.spring.mvc.learn.service;
+
+public interface DubboDemoService {
+    String sayHello(String name);
+}
---
+++ src/main/java/net/zhangxiaoxiao/spring/mvc/learn/serviceimp/DubboDemoServiceImpl.java
@@ -0,0 +1,17 @@
+package net.zhangxiaoxiao.spring.mvc.learn.serviceimp;
+
+import net.zhangxiaoxiao.spring.mvc.learn.service.DubboDemoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DubboDemoServiceImpl implements DubboDemoService {
+
+    private static final Logger log = LoggerFactory.getLogger(DubboDemoServiceImpl.class);
+
+    @Override
+    public String sayHello(String name) {
+
+        log.error("hello method");
+        return "Hello, " + name;
+    }
+}
