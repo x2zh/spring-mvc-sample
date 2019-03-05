2852f4c7d14694a714ab58655b2bf0da3eb70458
zhangxiaoxiao005 <zhangxiaoxiao005@ke.com>
2019-03-06 04:16:29 +0800

    update 添加测试demo

---
+++ src/test/java/net/zhangxiaoxiao/spring/mvc/learn/HelloServiceTest.java
@@ -0,0 +1,19 @@
+package net.zhangxiaoxiao.spring.mvc.learn;
+
+
+import net.zhangxiaoxiao.spring.mvc.learn.service.HelloService;
+import net.zhangxiaoxiao.spring.mvc.learn.serviceimp.HelloServiceImpl;
+import org.junit.jupiter.api.Test;
+
+import java.rmi.RemoteException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class HelloServiceTest { @Test
+    public void helloTest() throws RemoteException {
+        HelloService helloService = new HelloServiceImpl();
+        assertEquals(helloService.hello(), "zhangxiaoxiao");
+        assertEquals(1, 1);
+    }
+}
