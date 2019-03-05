a19b30a5d3ee2a4b472ae57617af69bb5db82a05
zhangxiaoxiao005 <zhangxiaoxiao005@ke.com>
2019-03-06 03:19:16 +0800

    update 添加了dubbo相关的依赖

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,27 @@ repositories {
 }
 
 dependencies {
+    /*dubbo依赖*/
+
+    // 通过telnet调用dubbo提供的服务时需要此依赖
+    compile("com.alibaba:fastjson:1.2.56")
+    // dubbo依赖
+    compile("org.apache.dubbo:dubbo:2.7.0") {
+        // spring-mvc会引入spring context依赖
+        exclude("org.springframework")
+    }
+
+    // zookeeper依赖，需要和注册中心的版本相同
+    compile("org.apache.zookeeper:zookeeper:3.4.12")
+
+    //zookeeper客户端，dubbo使用zookeeper作为注册中心时需要使用此依赖
+    compile("org.apache.curator:curator-recipes:4.0.1") {
+        // dubbo会引入netty依赖所以在这里排除
+        exclude("io.netty", "netty")
+        // 这里引入的版本可能和注册中心的版本不一致，所以排除，然后单独引入
+        exclude("org.apache.zookeeper", "zookeeper")
+    }
+
     compile("com.lmax:disruptor:3.4.2")
     compile("org.mybatis", "mybatis", "3.5.0")
     compile("org.springframework", "spring-webmvc", "5.1.3.RELEASE")
